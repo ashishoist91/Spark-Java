eff5e47daf8f8eaa2b75816f48dc2f35feec5c99
Ashish Singh <ashishoist91@gmail.com>
2018-05-20 22:22:07 +0530

    Update MySQLoHiveDataIngestion.java

--- MySQLoHiveDataIngestion.java
+++ MySQLoHiveDataIngestion.java
@@ -5,14 +5,14 @@ import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Properties;
 
 import org.apache.log4j.Logger;
-import org.apache.spark.SparkContext;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.sql.DataFrame;
-import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SQLContext;
 import org.apache.spark.sql.hive.HiveContext;
@@ -23,8 +23,11 @@ public class MySQLoHiveDataIngestion {
 	
 	private static final Logger logger = Logger.getLogger(MySQLoHiveDataIngestion.class.getName());
 	private static Connection conection = null;
+	private static Map<String,String> columnDetails= new HashMap<String,String>();
 	
 	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+		System.setProperty("hadoop.home.dir", "E:\\Hadoop");
+		
 		logger.info("Calling main Method");
 		logger.info("Creating Java Spark Context");
 		JavaSparkContext javaSparkContext = new JavaSparkContext("local", "MySQLoHiveDataIngestion");
@@ -32,11 +35,28 @@ public class MySQLoHiveDataIngestion {
 		//SparkContext sqlContext = new org.apache.spark.sql.SQLContext(javaSparkContext);
 		HiveContext hiveContext = new HiveContext(javaSparkContext);
 		Properties props = new Properties();
-		props.setProperty("user", "root");
-		props.setProperty("password", "root");
+		props.setProperty("user", "admin");
+		props.setProperty("password", "admin");
 		//Dataset<Row> result = hiveContext.read().jdbc("jdbc:mysql://localhost:3306/hadoop", "transaction", props);;
 		
 		DataFrame result = hiveContext.read().jdbc("jdbc:mysql://localhost:3306/hadoop", "transaction", props);
+		
+		Map<String, String> options = new HashMap<String, String>();
+		options.put("url", "jdbc:mysql://localhost:3306/hadoop");
+		options.put("dbtable", "hadoop.transaction");
+		options.put("user", "root");
+		options.put("password", "root");
+		
+		DataFrame frame = hiveContext.read().format("jdbc").options(options).load();
+		String columns [] = frame.columns();
+		for(String column : columns) {
+			logger.info("Column Name :  "+ column);
+			//logger.info("Data :  "+ frame.collectAsList().forEach(r -> System.out.println(r)));
+		}
+		
+		//frame.
+		
+		
 		//result.show();
 		//result.printSchema();
 		//Dataset<Row> newResult = result.wi
@@ -73,6 +93,7 @@ public class MySQLoHiveDataIngestion {
 		String createTable = "CREATE TABLE IF NOT EXISTS " + " transaction (";
 		ResultSetMetaData rsMetaData = rs.getMetaData();
 		for(int i=1;i<=rsMetaData.getColumnCount();i++) {
+			columnDetails.put(rsMetaData.getColumnName(i), rsMetaData.getColumnTypeName(i));
 			logger.info("Column Name " + rsMetaData.getColumnName(i));
 			logger.info("Column DataType " + rsMetaData.getColumnTypeName(i));
 			//logger.info("Column DataType " + rsMetaData.);
@@ -80,6 +101,8 @@ public class MySQLoHiveDataIngestion {
 			
 			createTable += rsMetaData.getColumnName(i) + " " +
 							getHiveDataType(rsMetaData.getColumnTypeName(i)) + ",";	
+			
+			
 		}
 		
 		createTable = createTable.trim().substring(0, createTable.length()-1) + ") STORED AS PARQUET" ;
